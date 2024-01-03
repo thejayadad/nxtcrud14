74f5717cce43a5f3f597feab9e76132eb548925a
thejayadad <74875595+thejayadad@users.noreply.github.com>
2024-01-03 10:01:27 -0500

    update

--- myapp/components/CreateForm.js
+++ myapp/components/CreateForm.js
@@ -1,9 +1,24 @@
 'use client'
 import React from 'react'
+import Input from './Input'
 
 const CreateForm = () => {
   return (
-    <div>CreateForm</div>
+    <div>
+        <form
+        className='bg-gray-300 p-2 rounded-md'
+        >
+            <div className='relative'>
+            <label for="name-with-label" class="text-gray-700">
+                Name
+            </label>
+            <Input
+                placeholder={'Name'}
+                
+            />
+            </div>
+        </form>
+    </div>
   )
 }
 
