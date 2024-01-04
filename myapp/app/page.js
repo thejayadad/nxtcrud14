78ff2b73a816883ccced44e385624e8c08f158c2
thejayadad <74875595+thejayadad@users.noreply.github.com>
2024-01-04 10:20:54 -0500

    update

--- myapp/app/page.js
+++ myapp/app/page.js
@@ -17,8 +17,39 @@ export default function Home() {
     <div className='flex flex-col px-4 py-4'>
     <span>Search</span>
 
-    <div className='bg-orange-200'>
-      contacts
+    <div className=' py-4'>
+      <table
+      className='table-auto w-full text-left whitespace-no-wrap'
+      >
+        <thead>
+          <tr>
+            <th 
+            className='px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl'>
+              First Name
+            </th>
+            <th
+            className='px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100'
+            >
+              Last Name
+            </th>
+            <th
+            className='px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100'
+            >
+            Email
+            </th>
+            <th
+            className='px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100'
+            >
+              Contact Number
+            </th>
+            <th
+            className='px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100'
+            >
+              Action
+            </th>
+          </tr>
+        </thead>
+      </table>
     </div>
     </div>
    </main>
